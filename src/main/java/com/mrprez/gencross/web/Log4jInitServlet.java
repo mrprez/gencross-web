075a50485849455427d44c83c36e2efd205e8218
MrPrez <gagneret@yahoo.fr>
2014-04-26 00:42:18 +0200

    Remove unused import

--- src/main/java/com/mrprez/gencross/web/Log4jInitServlet.java
+++ src/main/java/com/mrprez/gencross/web/Log4jInitServlet.java
@@ -2,7 +2,6 @@ package com.mrprez.gencross.web;
 
 import java.util.Enumeration;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 
