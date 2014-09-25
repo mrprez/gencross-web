a441eca6209356c81163482054114f4ff5c425fe
mrprez <gagneret@yahoo.fr>
2014-09-25 16:29:19 +0200

    Add plugin descriptor to filter on plugin name

--- src/main/java/com/mrprez/gencross/web/dao/face/ITemplateFileResource.java
+++ src/main/java/com/mrprez/gencross/web/dao/face/ITemplateFileResource.java
@@ -3,10 +3,11 @@ package com.mrprez.gencross.web.dao.face;
 import java.io.File;
 
 import com.mrprez.gencross.export.TemplatedFileGenerator;
+import com.mrprez.gencross.disk.PluginDescriptor;
 
 public interface ITemplateFileResource {
 
-	public File[] getTemplateFiles(Class<? extends TemplatedFileGenerator> clazz) throws Exception;
+	public File[] getTemplateFiles(Class<? extends TemplatedFileGenerator> generatorClazz, PluginDescriptor pluginDescriptor) throws Exception;
 
-	public File getTemplateFile(Class<? extends TemplatedFileGenerator> clazz, String fileName) throws Exception;
+	public File getTemplateFile(Class<? extends TemplatedFileGenerator> generatorClazz, PluginDescriptor pluginDescriptor, String fileName) throws Exception;
 }
